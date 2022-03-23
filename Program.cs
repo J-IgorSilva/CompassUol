deee8929fe645ffd3bebd0f8b6a92a3c21943aa6
J-IgorSilva <aleatoria8821@gmail.com>
2022-03-22 22:31:51 -0300

    1009 Salario Bônus

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+
+namespace sprint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            double salario, vendas, total;
+
+            nome = Console.ReadLine();
+            salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            vendas = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            total = vendas * 15.0 / 100 + salario;
+
+            Console.WriteLine("TOTAL = R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
+        }  
+    }
+}
