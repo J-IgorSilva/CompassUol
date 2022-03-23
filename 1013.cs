0d49bcb70024b54b4a59249f81b69b1c53eb86bb
J-IgorSilva <aleatoria8821@gmail.com>
2022-03-22 23:18:33 -0300

    1013 O Maior

---
+++ 1013.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+
+namespace sprint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] valorEntrada = Console.ReadLine().Split(' ');
+
+            int A = int.Parse(valorEntrada[0]);
+
+            int B = int.Parse(valorEntrada[1]);
+
+            int C = int.Parse(valorEntrada[2]);
+
+            int maiorAB = ((A + B + Math.Abs(A - B)) / 2); 
+
+            Console.WriteLine(((maiorAB + C + Math.Abs(maiorAB - C)) / 2) + " eh o maior");
+
+        }
+    }
+}
