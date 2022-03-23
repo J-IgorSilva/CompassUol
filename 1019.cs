5956a3dc1b33a805ef3311a091f199e0ec0e6bc6
J-IgorSilva <aleatoria8821@gmail.com>
2022-03-22 23:24:57 -0300

    1019 Conversão de Tempo

---
+++ 1019.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+
+namespace sprint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valorInicial, restoTempo, horas, minutos, segundos;
+
+            valorInicial = int.Parse(Console.ReadLine());
+
+
+            horas = valorInicial / 3600;
+            restoTempo = valorInicial % 3600;
+
+
+            minutos = restoTempo / 60;
+            restoTempo = restoTempo % 60;
+            segundos = restoTempo;
+            Console.WriteLine(horas + ":" + minutos + ":" + segundos);
+        }
+    }
+}
