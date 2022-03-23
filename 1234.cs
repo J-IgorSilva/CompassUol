82450a2cd9fd9474cb6b8a915cd1c30e96edaf17
J-IgorSilva <aleatoria8821@gmail.com>
2022-03-23 00:06:10 -0300

    1234 Sentença Dançante

---
+++ 1234.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+
+namespace sprint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string entradaTexto;
+            while ((entradaTexto = Console.ReadLine()) != null)
+            {
+                var arrayTexto = entradaTexto.ToCharArray();
+                var ultimaMaiuscula = false;
+
+                var resultadoTexto = "";
+
+                for (int i = 0; i < arrayTexto.Length; i++)
+                {
+                    if (!char.IsWhiteSpace(arrayTexto[i]))
+                    {
+                        resultadoTexto += ultimaMaiuscula ? char.ToLower(arrayTexto[i]) : char.ToUpper(arrayTexto[i]);
+                        ultimaMaiuscula = !ultimaMaiuscula;
+                    }
+                    else
+                        resultadoTexto += arrayTexto[i];
+                }
+
+                Console.WriteLine(resultadoTexto);
+            }
+        }
+    }
+}
