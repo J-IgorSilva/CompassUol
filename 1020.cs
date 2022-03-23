c75a223ed3b2facb100b921b27dd090704e3ec61
J-IgorSilva <aleatoria8821@gmail.com>
2022-03-22 23:27:33 -0300

    1020 Idade em Dias

---
+++ 1020.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+
+namespace sprint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade, ano, mes, dias;
+
+            idade = int.Parse(Console.ReadLine());
+
+            ano = idade / 365;
+            mes = (idade % 365) / 30;
+            dias = (idade % 365) % 30;
+
+            Console.WriteLine(ano + " ano(s)\n" + mes + " mes(es)\n" + dias + " dia(s)");
+        }
+    }
+}
