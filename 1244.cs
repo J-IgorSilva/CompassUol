38393891da1c7f64f95f8343b9c24605dc6f8b8e
J-IgorSilva <aleatoria8821@gmail.com>
2022-03-29 23:12:55 -0300

    1244 Ordenação Por Tamanho

---
+++ 1244.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace sprint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			var testeNumeros = int.Parse(Console.ReadLine());
+			var lordemDaLinha = new List<string>();
+
+			for (int n = 0; n < testeNumeros; n++)
+			{
+				var linha = Console.ReadLine().Split(' ').ToList();
+				linha = linha.OrderByDescending(x => x.Length).ToList();
+				lordemDaLinha.Add(string.Join(" ", linha.ToArray())); // metodo para concatenar e ordenar
+			}
+
+			lordemDaLinha.ForEach(x => Console.WriteLine((x))); // comando para cada elemento lista
+			
+		}
+	}
+}
