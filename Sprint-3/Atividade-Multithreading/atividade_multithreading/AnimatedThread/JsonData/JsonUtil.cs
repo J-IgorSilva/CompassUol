4c7c7ad0b19e3bab715a6338c91f0851268f4012
J-IgorSilva <aleatoria8821@gmail.com>
2022-06-03 23:52:00 -0300

    Atividade 2 - Multithreading

---
+++ Sprint-3/Atividade-Multithreading/atividade_multithreading/AnimatedThread/JsonData/JsonUtil.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimatedThread.JsonData
+{
+    public static class JsonUtil
+    {
+        public static void ExportJsonToFile(object obj)
+        {
+            string path = $"..\\..\\..\\..\\files\\result - {DateTime.Now:dd.MM.yyyy HH.mm.ss}.json";
+            File.Create(path).Close();
+
+            using var file = new StreamWriter(path, false, Encoding.UTF8);
+            file.WriteLine(JsonConvert.SerializeObject(obj, Formatting.Indented));
+        }
+    }
+}
